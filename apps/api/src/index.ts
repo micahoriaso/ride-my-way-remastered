be0831fa103447e186a73ac97d2b968fc7a97f39
micahoriaso <micahoriaso@gmail.com>
2018-12-26 12:26:53 +0300

    add migrations

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -40,22 +40,6 @@ createConnection()
     // start express server
     app.listen(port);
 
-    // insert new users for test
-    // await connection.manager.save(
-    //   connection.manager.create(User, {
-    //     firstName: "Timber",
-    //     lastName: "Saw",
-    //     age: 27
-    //   })
-    // );
-    // await connection.manager.save(
-    //   connection.manager.create(User, {
-    //     firstName: "Phantom",
-    //     lastName: "Assassin",
-    //     age: 24
-    //   })
-    // );
-
     console.log(
       "Express server has started on port 5000. Open http://localhost:5000/api to see results"
     );
